3064e7404db665cbcef43c0da4bf40d48ee6b5a4
Jardel Sousa <Jarbasrt81@gmail.com>
2022-12-01 10:24:37 -0300

    working

---
+++ config/config.js
@@ -0,0 +1,18 @@
+module.exports = {
+
+  Prefix: ".", // YOUR BOT PREFIX.
+
+  Users: {
+    OWNERS: ["479733668559192066"] // THE BOT OWNERS ID.
+  },
+
+  Handlers: {
+    MONGO: "mongodb+srv://smollkyadmin:admin@supremobot.uli7tvb.mongodb.net/?retryWrites=true&w=majority" // YOUR MONGO URI. (USE THIS ONLY IN VSCODE)
+  },
+
+  Client: {
+    TOKEN: "OTEwMTkwNzYwMzYyNjM5Mzgx.GDME_X.zEfYSFZGbw__F0bFsFBD6uzXcQsFT_pfgImxck", // YOUR BOT TOKEN. (USE THIS ONLY IN VSCODE)
+    ID: "910190760362639381" // YOUR BOT ID.
+  }
+
+}
